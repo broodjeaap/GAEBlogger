fc411f088a8fe513e59fdc6e675e0b5db359b104
David van Zessen <davidzessen@gmail.com>
2011-12-31 16:10:20 +0100

    -

--- static/script.js
+++ static/script.js
@@ -1,17 +1,3 @@
-var openDiv = null;
-
-function reply(comment){
-	if(openDiv != null){
-		openDiv.remove();
-	}
-	link = $("#reply_"+comment);
-	link.html("cancel");
-	link.attr('onclick', "cancelReply('"+comment+"')");
-	div = $("#div_"+comment);
-	openDiv = div;
-	div.append("<div class='replyAppendDiv' id='divAppend_"+comment+"'> <form name='reply' action='/replypost' method='post'><input type='hidden' name='commentKey' value='"+comment+"' /><textarea name='commentBody' cols='50' rows='6'></textarea><br /><input type='submit' value='Reply' /></form></div>");
-}
-
 function deleteComment(comment){
 	var choice = confirm("Are you sure you want to delete your comment?");
 	if(choice){
@@ -32,6 +18,14 @@ function cancelEdit(comment){
 	link.html("edit");
 	link.attr('onclick', "editComment('"+comment+"')");
 	$("#divAppend_"+comment).remove();
+}
+
+function reply(comment){
+	link = $("#reply_"+comment);
+	link.html("cancel");
+	link.attr('onclick', "cancelReply('"+comment+"')");
+	div = $("#div_"+comment);
+	div.append("<div class='replyAppendDiv' id='divAppend_"+comment+"'> <form name='reply' action='/replypost' method='post'><input type='hidden' name='commentKey' value='"+comment+"' /><textarea name='commentBody' cols='50' rows='6'></textarea><br /><input type='submit' value='Reply' /></form></div>");
 }
 
 function cancelReply(comment){
